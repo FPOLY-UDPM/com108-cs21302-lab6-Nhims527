a9257de766c3e5e049bcf666242bcf8ee2354778
Nhims <quoccuongtran848@gmail.com>
2026-02-05 11:18:53 +0700

    lab6

--- bai2.c
+++ bai2.c
@@ -1,12 +1,36 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Trần Quốc Cường]
+ * MSSV:      [PS48626]
+ * Lớp:       [COM108-CS21302]
  *****************************************************************************/
 
 //  BÀI 2: TÌM GIÁ TRỊ LỚN NHẤT VÀ NHỎ NHẤT TRONG MẢNG 
 //  Input: Nhập vào từ bàn phím 1 mảng các số nguyên. Mảng gồm n phần tử 
 //  Output: Xuất ra màn hình giá trị nhỏ nhất và lớn nhất của mảng
 
-// VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    scanf("%d", &n);
+
+    int mang[n];
+    for (i = 0; i < n; i++) {
+        scanf("%d", &mang[i]);
+    }
+
+    int max = mang[0];
+    int min = mang[0];
+
+    for (i = 1; i < n; i++) {
+        if (mang[i] > max) max = mang[i];
+        if (mang[i] < min) min = mang[i];
+    }
+
+    printf("%d\n", max);
+    printf("%d\n", min);
+
+    return 0;
+}
+
 
--- bai4.c
+++ bai4.c
@@ -1,12 +1,35 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Trần Quốc Cường]
+ * MSSV:      [PS48626]
+ * Lớp:       [COM108-CS21302]
  *****************************************************************************/
 
 //  BÀI 4: TÍNH BÌNH PHƯƠNG CÁC PHẦN TỬ TRONG MẢNG 2 CHIỀU 
 //  Input: Nhập vào từ bàn phím 1 ma trận các số nguyên. Mảng gồm n hàng, m cột 
 //  Output: Xuất ra màn hình ma trận bình phương 
 
-// VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+
+int main() {
+    int n, m, i, j;
+    scanf("%d %d", &n, &m);
+
+    int mang[n][m];
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            scanf("%d", &mang[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("%d ", mang[i][j] * mang[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
 
